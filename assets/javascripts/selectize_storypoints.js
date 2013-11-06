6638a7e5d8dac43d3af6e9926efc51542364206a
Jacek Grzybowski <jgrzybowski@artegence.com>
2013-11-06 16:43:46 +0100

    ajax reload fix

--- assets/javascripts/selectize_storypoints.js
+++ assets/javascripts/selectize_storypoints.js
@@ -1,7 +1,14 @@
 $(function(){
-  var select_field = $('#temp_story_points');
+  change_to_select();
+  $(document).bind('ajaxSuccess', function() {
+    change_to_select();
+  });
+});
+
+function change_to_select(){
+  var select_field = $('#temp_story_points').clone();
   $('#issue_story_points').replaceWith(select_field);
   select_field.attr('id', 'issue_story_points').attr('name', 'issue[story_points]');
   select_field.removeAttr('style');
   select_field.val( $('#current_storypoint').text() );
-});
+};
